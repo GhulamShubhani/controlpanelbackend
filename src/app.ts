3b0448f967c698de7c4d7be5339ea68a80d21c01
GHULAM SHUBHANI <ghulam@payfinix.in>
2023-05-06 18:36:03 +0530

    test3

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import { config } from 'dotenv'
+config();
+
 import adminRouter from "./routes/Admin";
 import LandLordRouter from "./routes/LandLord";
 import PropertyRouter from "./routes/Property";
@@ -10,13 +13,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 8000;
+const server: string = process.env.CONNECT_DATABASE!
 
 app.use(adminRouter);
 app.use(LandLordRouter);
 app.use(PropertyRouter);
 
-mongoose.connect("mongodb://127.0.0.1:27017/gscControlpanel").then(() => {
+mongoose.connect(server).then(() => {
   app.listen(PORT, () => {
-    console.log(`server is started at ${PORT}`);
+    console.log(`server is started ${server} at ${PORT}`);
   });
 });
